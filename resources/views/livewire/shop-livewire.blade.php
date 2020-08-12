fa9502fb6c5afb1f8f69e96e1706fc67e30f4226
Bane Radosavljević <r_bane@hotmail.com>
2020-08-12 22:51:54 +0200

    Ispravljen bug na shop strani

--- resources/views/livewire/shop-livewire.blade.php
+++ resources/views/livewire/shop-livewire.blade.php
@@ -109,7 +109,7 @@
                                 <div class="flex flex-1 items-center mt-10 justify-between mr-4">
                                     <h3 class="uppercase text-lg">Veličine</h3>
                                     <div x-data="{otvori:true}" >
-                                        <div @click="otvori=!otvori" id="otvori" class="cursor-pointer">
+                                        <div @click="otvori=!otvori" id="otvori">
                                             <i x-show="otvori" class='fa fa-angle-double-up'></i>
                                             <i x-show="!otvori" class='fa fa-angle-double-down'></i>
                                             
@@ -117,46 +117,47 @@
                                     </div>
                                 </div>  
                                     
-                                <hr class="bg-boja h-1 w-32">
-                                <div id="all-sizes" class="w-32 h-auto rounded-lg mt-4"
-                                x-transition:enter="transition-transform transition-opacity ease-out duration-300"
-                                x-transition:enter-start="opacity-0 transform -translate-y-2"
-                                x-transition:enter-end="opacity-100 transform translate-y-0"
-                                x-transition:leave="transition ease-in duration-300"
-                                x-transition:leave-end="opacity-0 transform -translate-y-3">
-                                    <div class="flex flex-col  items-start  ml-4 justify-center " >
-                                        @foreach ($sizes as $size)
-                                        <div class="flex flex-col w-12 max-h-full" >
-                                            <label class="inline-flex items-center  text-sm">
+                                    <hr class="bg-boja h-1 w-32">
+                                    <div id="all-sizes" class="w-32 h-auto rounded-lg mt-4"
+                                    x-transition:enter="transition-transform transition-opacity ease-out duration-300"
+                                    x-transition:enter-start="opacity-0 transform -translate-y-2"
+                                    x-transition:enter-end="opacity-100 transform translate-y-0"
+                                    x-transition:leave="transition ease-in duration-300"
+                                    x-transition:leave-end="opacity-0 transform -translate-y-3">
+                                        <div class="flex flex-col  items-start  ml-4 justify-center " >
+                                            @foreach ($sizes as $size)
+                                            <div class="flex flex-col w-12 max-h-full" >
+                                                <label class="inline-flex items-center  text-sm">
+                                                    <input type="checkbox" class="form-checkbox h-3 w-3 text-gray-600 text-sm " id="{{$size->id}}" value="{{$size->id}}" wire:model="requestedSizes">
+                                                    <li class="list-none ml-2">{{$size->value}}</li>
+                                                </label>
+                                            </div>
+                                            @endforeach
+                                        </div>
+                                    
+                                     
+                        
+                                        <div x-data="{ open: false }" class="flex flex-col justify-center w-32 h-auto rounded-b-lg">
+                                            @foreach ($sizesAll as $size)
+                                        
+                                        <div class="flex flex-col max-h-full pl-4" x-show="open"
+                                        x-transition:enter="transition ease-out duration-400" x-transition:enter-start="opacity-0 transform scale-90" x-transition:enter-end="opacity-100 transform scale-100" x-transition:leave="transition ease-in duration-400" x-transition:leave-start="opacity-100 transform scale-100" x-transition:leave-end="opacity-0 transform scale-90"
+                                      >
+                                            <label class="inline-flex items-center text-sm">
                                                 <input type="checkbox" class="form-checkbox h-3 w-3 text-gray-600 text-sm " id="{{$size->id}}" value="{{$size->id}}" wire:model="requestedSizes">
                                                 <li class="list-none ml-2">{{$size->value}}</li>
                                             </label>
                                         </div>
+                                        
                                         @endforeach
-                                    </div>
-                                
-                                 
-                    
-                                    <div x-data="{ open: false }" class="flex flex-col justify-center w-32 h-auto rounded-b-lg">
-                                        @foreach ($sizesAll as $size)
+                                        
+                                        <button @click="open = !open" type="button" 
+                                        class="bg-boja hover:bg-orange-900 text-white mt-2 rounded"  x-html="open ? `Prikaži manje` :`Prikaži više`">
+                                        </button>
+                                        </div>
                                     
-                                    <div class="flex flex-col max-h-full pl-4" x-show="open"  
-                                    x-transition:enter="transition ease-out duration-400" x-transition:enter-start="opacity-0 transform scale-90" x-transition:enter-end="opacity-100 transform scale-100" x-transition:leave="transition ease-in duration-400" x-transition:leave-start="opacity-100 transform scale-100" x-transition:leave-end="opacity-0 transform scale-90"
-                                  >
-                                        <label class="inline-flex items-center text-sm">
-                                            <input type="checkbox" class="form-checkbox h-3 w-3 text-gray-600 text-sm " id="{{$size->id}}" value="{{$size->id}}" wire:model="requestedSizes">
-                                            <li class="list-none ml-2">{{$size->value}}</li>
-                                        </label>
-                                    </div>
-                                    
-                                    @endforeach
-                                    
-                                    <button @click="open = !open" type="button" 
-                                    class="bg-boja hover:bg-orange-900 text-white mt-2 rounded"  x-html="open ? `Prikaži manje` :`Prikaži više`">
-                                    </button>
-                                    </div>
-                                
-                                 </div>  
+                                     </div>  
+
 
 
                             <div x-data="{otvori:true}">
