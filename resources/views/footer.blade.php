e9dc3eff95e554eaa19a8e7d6d59206b3d2abb0b
Bane Radosavljević <r_bane@hotmail.com>
2020-08-14 17:12:41 +0200

    Footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -3,11 +3,11 @@
 @section('footer')
 
 
-    <div class="w-full p-8"  style="background-color: rgb(20, 104, 107);padding-left:40px">
-        <div class="sm:flex mb-0">
-      <div class="sm:w-1/3 h-auto ml-20 md:ml-8 mt-4 sm:mt-12 md:mt-8">
+    <div class="w-full py-8"  style="background-color: rgb(20, 104, 107);">
+        <div class="flex flex-col md:flex-row items-center md:items-start container">
+      <div class="md:w-1/3 h-auto">
           <ul class="list-reset leading-normal">
-              <div class="flex items-center ">
+              <div class="flex items-center">
                   <a href=""><li class="hover:text-gray-500"><img  src="{{asset('./img/baby.svg')}}" alt="kolica za bebe" style="" class="w-12 h-12 bg-transparent p-2 rounded-md hover:bg-gray-500">  </li></a>
               <a href="" class="ime"><li class="text-xl sm:text-2xl xl:text-3xl text-white ml-2 font-semibold  mb-2" >Mob-ing</li></a>
               </div>
@@ -18,10 +18,8 @@
             
           </ul>
       </div>
-      <div class="sm:w-1/3 h-auto ml-20 md:ml-8">
-        
-        
-          <ul class="list-reset leading-normal mt-4 sm:mt-12 md:mt-8">
+      <div class="md:w-1/3 h-auto">  
+          <ul class="list-reset leading-normal mt-8">
             <a href="{{route('about')}}"><li class="text-white text-base xl:text-xl hover:text-gray-500 "><i class="fa fa-angle-double-left  mr-2" aria-hidden="true"></i>O nama</li></a>
           <a href="{{route('porucivanje')}}"><li class="text-white text-base xl:text-xl hover:text-gray-500"><i class="fa fa-angle-double-left  mr-2 " aria-hidden="true"></i>Uputstvo za kupovinu</li></a>
           <a href="{{route('placanje')}}"><li class="text-white text-base xl:text-xl hover:text-gray-500"><i class="fa fa-angle-double-left  mr-2 " aria-hidden="true"></i>Plaćanje</li></a>
@@ -32,13 +30,11 @@
           </ul>
     
       </div>
-      <div class=" sm:w-1/3 h-auto sm:mt-0 mt-8 ml-12 md:ml-8">
-        <div class="">
-            <p class="text-center text-white text-base hover:text-gray-500 p-2 mr-24">Ovde nas možete pronaći:</p>
+      <div class="md:w-1/3 h-auto">
+            <p class="text-white text-base hover:text-gray-500 pb-2 text-center mt-8 md:mt-0">Ovde nas možete pronaći:</p>
             {{-- <iframe  class="mapa w-64 h-56 md:w-96 md:h-64 pb-2" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1398.1260028649447!2d19.257321988241856!3d45.50500542734718!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ssr!2srs!4v1594243180190!5m2!1ssr!2srs"  frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe> --}}
-            <div class="mapouter"><div class="gmap_canvas"><iframe class="w-72 lg:w-96 " width="384" height="256" id="gmap_canvas" src="https://maps.google.com/maps?q=Kneza%20Mihaila%2021%2C%20%D0%9E%D1%9F%D0%B0%D1%86%D0%B8&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net/blog/divi-discount-code-elegant-themes-coupon/"></a></div><style>.mapouter{position:relative;text-align:right;height:256px;width:384px;}.gmap_canvas {overflow:hidden;background:none!important;height:256px;width:384px;}</style></div>
-        </div>  
-    
+            <div class="mapouter"><div class="gmap_canvas"><iframe class="mx-auto md:ml-12 w-72 lg:w-96 " width="384" height="256" id="gmap_canvas" src="https://maps.google.com/maps?q=Kneza%20Mihaila%2021%2C%20%D0%9E%D1%9F%D0%B0%D1%86%D0%B8&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net/blog/divi-discount-code-elegant-themes-coupon/"></a></div><style>.mapouter{position:relative;text-align:right;height:256px;width:384px;}.gmap_canvas {overflow:hidden;background:none!important;height:256px;width:384px;}</style></div>
+        
       </div>
        
     
