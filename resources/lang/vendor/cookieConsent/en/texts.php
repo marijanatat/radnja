9cb6a542c96f8028cc61e3e719c7540dc983bb44
Marijana Tatalovic <tatalovicmarijana@gmail.com>
2020-08-28 01:00:16 +0200

    kolacici

---
+++ resources/lang/vendor/cookieConsent/en/texts.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'message' => 'Ovaj sajt koristi kolačiće radi što boljeg iskustva posetilaca. Ukoliko nastavite sa korišćenjem sajta, smatra se da ste saglasni sa našim uslovima korišćenja kolačića i potvrđujete prihvatanje kolačića na njegovim stranicama.',
+    'agree' => 'Slažem se',
+];
---
+++ resources/lang/vendor/cookieConsent/sr/text.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'message' => '
+    Poštovani korisniče, naš sajt koristi cookies (kolačiće) u cilju poboljšanja korisničkog iskustva. Ukoliko nastavite da pregledate i koristite našu Internet prodavnicu slažete se sa upotrebom kolačića.',
+    'agree' => 'Slažem se',
+];
