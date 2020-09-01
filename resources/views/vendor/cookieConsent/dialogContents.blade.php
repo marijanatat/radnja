c902ace9fefc1a46676a7be899178dbefbe15f44
Branimir Radosavljevic <rbranimir@ymail.com>
2020-09-01 12:37:04 +0200

    Cookie consent page

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -1,11 +1,11 @@
-<div class="w-full js-cookie-consent cookie-consent fixed bottom-0">
+<div class="w-full js-cookie-consent cookie-consent fixed bottom-0 z-50">
 
-    <div class="flex justify-around items-center bg-gray-800 text-gray-100 p-3" style="opacity: 90%">
+    <div class="flex flex-col md:flex-row flex-wrap justify-around items-center bg-gray-800 text-gray-100 p-3">
         <div class="cookie-consent__message">
             {!! trans('cookieConsent::texts.message') !!}
         </div>
     
-        <button class="bg-orange-600 p-2 mr-8 focus:outline-none shadow-lg rounded-sm js-cookie-consent-agree cookie-consent__agree ">
+        <button class="bg-orange-600 p-2 focus:outline-none shadow-lg rounded-sm js-cookie-consent-agree cookie-consent__agree ">
             {{ trans('cookieConsent::texts.agree') }}
         </button>
     </div>
