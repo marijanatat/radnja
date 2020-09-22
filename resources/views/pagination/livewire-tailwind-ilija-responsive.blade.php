ad16441a797da32fd80c3d73e319be2bbbf1d511
Branimir Radosavljevic <rbranimir@ymail.com>
2020-09-22 13:36:47 +0200

    Promenjena paginacija

--- resources/views/pagination/livewire-tailwind-ilija-responsive.blade.php
+++ resources/views/pagination/livewire-tailwind-ilija-responsive.blade.php
@@ -1,19 +1,10 @@
 @if ($paginator->hasPages())
-    <div class="flex my-2 text-sm items-center">
-        <div class="mr-2">
-            <p class="text-sm leading-5 text-gray-900">
-                <span class="font-medium">{{ $paginator->firstItem() }}</span>
-                {!! __('do') !!}
-                <span class="font-medium">{{ $paginator->lastItem() }}</span>
-                {!! __('od') !!}
-                <span class="font-medium">{{ $paginator->total() }}</span>
-                {!! __('proizvoda') !!}
-            </p>
-        </div>        
+    <div class="flex items-end my-2 text-sm">
+        
         @if ( ! $paginator->onFirstPage())
             {{-- First Page Link --}}
             <a
-            class="text-boja mx-1 px-2 py-2 font-bold text-center hover:text-teal-500 cursor-pointer" title="Prva strana"
+            class="text-boja mx-1 px-2 py-2 font-bold text-center hover:text-teal-500 cursor-pointer"
             wire:click="gotoPage(1)"
             >
             <<
@@ -21,13 +12,13 @@
             @if($paginator->currentPage() > 2)
             {{-- Previous Page Link --}}
             <a
-                class="mx-1 px-2 py-2 text-boja font-bold text-center hover:text-teal-500 cursor-pointer" title="Prethodna strana"
+                class="mx-1 px-2 py-2 text-boja font-bold text-center hover:text-teal-500 cursor-pointer"
                 wire:click="previousPage"
             >
             <
             </a>
             @endif
-        @endif        
+        @endif
 
         <!-- Pagination Elements -->
         @foreach ($elements as $element)
@@ -46,7 +37,7 @@
                     <!--  Show active page two pages before and after it.  -->
                     @if ($page == $paginator->currentPage())
                         <span class="mx-1 px-3 py-1 border border-boja bg-boja text-white font-semibold text-center hover:bg-teal-500 hover:border-teal-500 rounded-md cursor-pointer">{{ $page }}</span>
-                        @elseif ($page === $paginator->currentPage() + 1 || $page === $paginator->currentPage() + 2 || $page === $paginator->currentPage() - 1 || $page === $paginator->currentPage() - 2)
+                    @elseif ($page === $paginator->currentPage() + 1 || $page === $paginator->currentPage() - 1)
                         <a class="mx-1 px-3 py-1 border border-boja text-boja font-semibold text-center hover:text-teal-500 rounded-md cursor-pointer" wire:click="gotoPage({{$page}})">{{ $page }}</a>
                     @endif
 
@@ -65,14 +56,14 @@
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
             @if($paginator->lastPage() - $paginator->currentPage() >= 2)
-                <a class="text-boja mx-1 px-2 py-2 font-bold text-center hover:text-teal-500 cursor-pointer" title="Sledeća strana"
+                <a class="text-boja mx-1 px-2 py-2 font-bold text-center hover:text-teal-500 cursor-pointer"
                 wire:click="nextPage"
                 rel="next">
                 >
                 </a>
             @endif
             <a
-                class="text-boja mx-1 px-2 py-2 font-bold text-center hover:text-teal-500 cursor-pointer" title="Poslednja strana"
+                class="text-boja mx-1 px-2 py-2 font-bold text-center hover:text-teal-500 cursor-pointer"
                 wire:click="gotoPage({{ $paginator->lastPage() }})"
             >
             >>
